fa7c7cacd3618d8ba8515210d2f67b735670a297
AstroKhet <liugaoyuan123@gmail.com>
2025-12-20 17:59:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
-from gui.app import KhetcoinApp
-from setup.setup import SetupApp
-from networking.node import Node
-from setup.functions import INITIAL_SETUP, RUNTIME_SETUP
-from utils.config import APP_CONFIG
-
-node_kwargs = {"name": "Khet", "host": "127.0.0.1", "port": 8888}
-# node_kwargs = {"name": APP_CONFIG.get("node", "name"), "host": APP_CONFIG.get("node", "name")}
 
 def main():
+    from utils.config import APP_CONFIG
+
     if not APP_CONFIG.get("app", "initial_setup"):
+        from setup.setup import SetupApp
+        from setup.functions import INITIAL_SETUP, RUNTIME_SETUP
+
         setup_app = SetupApp()
         setup_app.main()
         INITIAL_SETUP()
-    
-        # Run the initial setup sequence
+
     RUNTIME_SETUP()
-    
+
+    from gui.app import KhetcoinApp
+
+    node_kwargs = {
+        "name": APP_CONFIG.get("node", "name"),
+        "host": "0.0.0.0",
+        "port": APP_CONFIG.get("node", "port")
+    }
+
     app = KhetcoinApp(**node_kwargs)
-    
-    print("main")
     app.main()
-    print("main finish")
 
 
 if __name__ == "__main__":
